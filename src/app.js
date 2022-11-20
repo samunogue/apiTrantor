485f8c43ca464a0c53c27957f14d9cb0056a4b10
samunogue <samuel.nogueira716@gmail.com>
2022-11-20 14:53:37 -0300

    adicionando token aos headers permitidos

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
         res.header("Access-Control-Allow-Origin", "*")
         res.header(
           "Access-Control-Allow-Headers",
-          "Origin, X-Requested, Content-Type, Accept"
+          "Origin, X-Requested, Content-Type, Accept, token"
         )
         res.header(
                 "Access-Control-Allow-Methods",
